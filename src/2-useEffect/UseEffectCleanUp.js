340271d7b1ff218aeb16b2b95f4e7f6f8943089d
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-06 21:58:31 +0530

    useEffect - Cleanup function - logic added

--- src/2-useEffect/UseEffectCleanUp.js
+++ src/2-useEffect/UseEffectCleanUp.js
@@ -1,11 +1,35 @@
 import React from "react";
 
 const UseEffectCleanUp = () => {
+
+    const [windowSize, setWindowSize] = React.useState(window.innerWidth)
+
+    const changeWindowSizeValue = () => {
+        setWindowSize(window.innerWidth)
+    }
+
+    React.useEffect(() => {
+        console.log("useEffect Called");
+        window.addEventListener("resize", changeWindowSizeValue)
+
+        return (() => {
+            console.log("Cleanup Called");
+            window.removeEventListener("resize", changeWindowSizeValue)
+        });
+    });
+
+    console.log("Render");
+
     return (
         <React.Fragment>
             <h1>
                 useEffect Hook - Clean up
             </h1>
+
+            <h1>
+                Window Size: {windowSize}
+            </h1>
+
         </React.Fragment>
     );
 }
