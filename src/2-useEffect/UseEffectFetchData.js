003b1f755afb7dfcddb178e335e953feeb81b4fb
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-07 22:15:02 +0530

    useEffect - fetch data -- Github User List API call and consol log -- DONE

--- src/2-useEffect/UseEffectFetchData.js
+++ src/2-useEffect/UseEffectFetchData.js
@@ -1,11 +1,33 @@
 import React from "react"
 
+const restUrl = "https://api.github.com/users"
+
 const UseEffectFetchData = () => {
+
+    const [users, setUsers] = React.useState([]);
+
+    const getUserData = async () => {
+        const response = await fetch(restUrl);
+        const userData = await response.json()
+        console.log(userData)
+    }
+
+    React.useEffect(() => {
+        getUserData()
+    });
+
     return (
         <React.Fragment>
             <h1>
                 useEffect Hook - Fetch Data
             </h1>
+
+            <br />
+
+            <h2>
+                Github Users
+            </h2>
+
         </React.Fragment>
     );
 }
