fd01c5a53c2eb87907a52acf855f9076e50142b7
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-07 21:50:41 +0530

    useEffect hook - fetch data - basic setup created

---
+++ src/2-useEffect/UseEffectFetchData.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const UseEffectFetchData = () => {
+    return (
+        <React.Fragment>
+            <h1>
+                useEffect Hook - Fetch Data
+            </h1>
+        </React.Fragment>
+    );
+}
+
+export default UseEffectFetchData;
--- src/App/UseEffectLearn.js
+++ src/App/UseEffectLearn.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import UseEffectBasics from "../2-useEffect/UseEffectBasics";
 import UseEffectCleanUp from "../2-useEffect/UseEffectCleanUp";
+import UseEffectFetchData from "../2-useEffect/UseEffectFetchData";
 
 const UseEffectLearn = () => {
     return (
@@ -24,6 +25,13 @@ const UseEffectLearn = () => {
                 <center>
                     <UseEffectCleanUp />
                 </center>
+
+                <hr />
+
+                <center>
+                    <UseEffectFetchData />
+                </center>
+
             </div>
         </React.Fragment>
     );
