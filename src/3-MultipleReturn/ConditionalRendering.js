86948a8a2825c12846d35888679da8fd17301d78
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-11 15:19:49 +0530

    Basic Example of conditional rendering -- added

--- src/3-MultipleReturn/ConditionalRendering.js
+++ src/3-MultipleReturn/ConditionalRendering.js
@@ -2,6 +2,30 @@ import React from "react";
 
 const ConditionalRendering = () => {
 
+    const [loading, setLoading] = React.useState(true)
+
+    React.useEffect(() => {
+        console.log("use effect called")
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000)
+    }, []
+    );
+
+    if (loading) {
+        return (
+            <React.Fragment>
+                <div className="container">
+                    <center>
+                        <h3>
+                            Loading ...
+                        </h3>
+                    </center>
+                </div>
+            </React.Fragment>
+        );
+    }
+
     return (
         <React.Fragment>
             <div className="container">
@@ -13,6 +37,8 @@ const ConditionalRendering = () => {
             </div>
         </React.Fragment>
     );
+
+
 }
 
 export default ConditionalRendering
