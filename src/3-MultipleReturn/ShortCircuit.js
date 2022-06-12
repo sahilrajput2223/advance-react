89c78755c195742f10d4ff0e97abf449efc2ec2c
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-12 15:59:18 +0530

    Short Circuit - Btn Change  Example Added

--- src/3-MultipleReturn/ShortCircuit.js
+++ src/3-MultipleReturn/ShortCircuit.js
@@ -3,8 +3,10 @@ import React from "react"
 const ShortCircuit = () => {
 
     const [text, setText] = React.useState('')
+    const [isError, setIsError] = React.useState(false)
     const firstShortCircuit = text || 'Hello World' // if first value is flase/empty then it will return second value else it will return first value
     const secondShortCircuit = text && 'Hello World'  // if first value is flase/empty then it will return first value else it will return second value
+
     return (
         <React.Fragment>
             <h3>
@@ -16,7 +18,10 @@ const ShortCircuit = () => {
             <h4>{text || "Default Text"}</h4>  {/**we are adding UI - but text will set based on condition */}
             {text && <h4>Default Text sdfsdf</h4>} {/** we are not adding ui based on data */}
             {!text && <h4>Default Text sdfsf</h4>}
-
+            <button className="btn btn-primary" onClick={() => { setIsError(!isError) }}>
+                Toggle Error
+            </button>
+            {isError && <h4>Error...</h4>}
         </React.Fragment>
     );
 }
