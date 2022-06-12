a808c70a93bd7a095d3ea425b6c10f99c72cba05
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-12 15:14:41 +0530

    Short Circuit - Example Added

--- src/3-MultipleReturn/ShortCircuit.js
+++ src/3-MultipleReturn/ShortCircuit.js
@@ -1,11 +1,22 @@
 import React from "react"
 
 const ShortCircuit = () => {
+
+    const [text, setText] = React.useState('')
+    const firstShortCircuit = text || 'Hello World' // if first value is flase/empty then it will return second value else it will return first value
+    const secondShortCircuit = text && 'Hello World'  // if first value is flase/empty then it will return first value else it will return second value
     return (
         <React.Fragment>
             <h3>
                 Short Circuit
             </h3>
+            <h4>First Condition: {firstShortCircuit} </h4>
+            <h4>Second Condition: {secondShortCircuit} </h4>
+
+            <h4>{text || "Default Text"}</h4>  {/**we are adding UI - but text will set based on condition */}
+            {text && <h4>Default Text sdfsdf</h4>} {/** we are not adding ui based on data */}
+            {!text && <h4>Default Text sdfsf</h4>}
+
         </React.Fragment>
     );
 }
