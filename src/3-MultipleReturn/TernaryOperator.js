fbda61516ac522b89b42fae3f1de7550999d34ec
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-15 14:06:31 +0530

    Ternary Operator - Basic Example Added

--- src/3-MultipleReturn/TernaryOperator.js
+++ src/3-MultipleReturn/TernaryOperator.js
@@ -2,9 +2,18 @@ import React from "react"
 
 const TernaryOperator = () => {
 
+    const [isError, setIsError] = React.useState(false)
+
     return (
         <React.Fragment>
             <h3>Ternary Operator</h3>
+
+            {isError ? <h4>Yes, it's Error</h4> : <h4>No Error, Good to go</h4>}
+
+            <button className="btn btn-primary" onClick={() => { setIsError(!isError) }}>
+                Change Error State
+            </button>
+
         </React.Fragment>
     );
 
