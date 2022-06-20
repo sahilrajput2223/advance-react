96d2ec6ee62ed99df4c71298e2a3ca51d0852a10
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-06-20 13:48:41 +0530

    Show Hide UI Based on Toggle and cleanup function Logic Added

--- src/3-MultipleReturn/ShowHideWithToggleAndCleanup.js
+++ src/3-MultipleReturn/ShowHideWithToggleAndCleanup.js
@@ -2,12 +2,45 @@ import React from "react"
 
 const ShowHideWithToggleAndCleanup = () => {
 
+    const [isShow, setIsShow] = React.useState(false)
+
+    const showHide = (isShow) => {
+        setIsShow(isShow)
+    }
+
     return (
         <React.Fragment>
             <h3> Show Hide Based On Toggle and Cleanup function</h3>
+
+            <button className="btn btn-primary" onClick={() => showHide(!isShow)}>Show / Hide</button>
+            {isShow && <Item />}
         </React.Fragment>
     );
+}
 
+const Item = () => {
+
+    const [size, setSize] = React.useState(window.innerWidth);
+
+    const checkAndUpdateSize = () => {
+        setSize(window.innerWidth)
+    }
+
+    React.useEffect(() => {
+        window.addEventListener('resize', checkAndUpdateSize)
+
+        //need to call clean up function to remove event listener so on every toggel we don't call resize event
+        return (() => {
+            window.removeEventListener('resize', checkAndUpdateSize)
+        });
+
+    }, []);
+
+    return (
+        <React.Fragment>
+            <h4>Window Size: {size} px</h4>
+        </React.Fragment>
+    );
 }
 
 export default ShowHideWithToggleAndCleanup
