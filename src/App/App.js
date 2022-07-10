c9d79f3e0ff882911420f6a29e3b059803d97a77
Sahil Rajput <rajputsahil.2204@gmail.com>
2022-07-10 14:18:59 +0530

    Basic Forms Setup Created

--- src/App/App.js
+++ src/App/App.js
@@ -2,17 +2,19 @@ import React from "react"
 
 //import UseStateLearn from "./UseStateLearn";
 //import UseEffectLearn from "./UseEffectLearn";
+//import MultipleReturn from "./MultipleReturnLearn";
 
 import "../App/app.css"
-import MultipleReturn from "./MultipleReturnLearn";
+import Forms from "./Forms";
 
 
 const App = () => {
     return (
         <React.Fragment>
             {/* <UseStateLearn />
-            <UseEffectLearn />*/}
-            <MultipleReturn />
+            <UseEffectLearn />
+            <MultipleReturn />*/}
+            <Forms />
         </React.Fragment>
     );
 }
---
+++ src/App/Forms.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Forms = () => {
+    return (
+        <React.Fragment>
+            <div className="container">
+                <center>
+                    <h1>React Forms</h1>
+                </center>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default Forms
